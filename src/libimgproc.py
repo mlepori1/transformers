a05b034b5eda69ee44be468d844fef68aa785c68
Jonathan Jones <jones.jonathan.david@gmail.com>
2016-06-13 23:03:33 -0400

    infer metric pos/rot and draw in blender

--- src/libimgproc.py
+++ src/libimgproc.py
@@ -8,6 +8,7 @@ AUTHOR
   Jonathan D. Jones
 """
 
+import csv
 import os
 import subprocess
 
@@ -133,14 +134,50 @@ def calibrateCamera(p_pixel, z_m):
 
 if __name__ == '__main__':
     
-    trial_id = 4
+    #plt.ioff()
+    
+    trial_id = 5
     
     c = DuploCorpus()
         
-    rgb_frame_fns = c.getRgbFrameFns(trial_id)#[50:51]
-    depth_frame_fns = c.getDepthFrameFns(trial_id)#[50:51]
+    rgb_frame_fns = c.getRgbFrameFns(trial_id)[50:51]
+    depth_frame_fns = c.getDepthFrameFns(trial_id)[50:51]
+    
+    # (Camera calibration)
+    block_1_corners = np.array([[120, 101], [119, 87], [133, 85], [134, 100]])
+    block_2_corners = np.array([[134, 100], [133, 85], [147, 84], [148, 99]])
+    block_3_corners = np.array([[149, 97], [148, 83], [161, 82], [162, 99]])
+    block_4_corners = np.array([[164, 96], [163, 81], [176, 80], [177, 95]])
+    side_lens = np.zeros(block_1_corners.shape[0])
+    side_lens[:-1] = (np.diff(block_1_corners, axis=0) ** 2).sum(axis=1) ** 0.5
+    side_lens[-1] = ((block_1_corners[-1,:] - block_1_corners[0,:]) ** 2).sum() ** 0.5
+    z_m = 600   # mm
+    
+    # Side length of a 2x2 DUPLO brick (from stack excange link below)
+    # http://bricks.stackexchange.com/questions/1588/
+    #   what-are-the-exact-dimensions-of-a-duplo-brick
+    block_width_metric = 31.8  # mm
+    block_height_metric = 19.2 # mm
+    # Derived length of a 4x2 rectangular DUPLO brick
+    block_length_metric = 2 * block_width_metric
+    
+    # Average focal length assuming a pinhole camera
+    f = (side_lens * (z_m - block_height_metric) / block_width_metric).mean() # pixels
+    
+    # Block corner locations in 3D assuming coordinate frames of metric space
+    # and the image plane are aligned
+    metric_corners = block_1_corners * (z_m - block_height_metric) / f
+    
+    # It looks like a model for the depth camera is:
+    #   depth = max_depth - intensity
+    # where max_depth is about 636
+    depth_offset = 636
+    # (end camera calibration)
     
     for rgb_path, depth_path in zip(rgb_frame_fns, depth_frame_fns):
+        
+        _, frame_fn = os.path.split(rgb_path)
+        frame_num = int(os.path.splitext(frame_fn)[0])
         
         # Read in frames and plot
         rgb_frame = io.imread(rgb_path)
@@ -159,11 +196,18 @@ if __name__ == '__main__':
         cv2.imwrite(os.path.join(c.paths['working'], fn), meta_frame[:,:,])
         """
         
-        plt.figure()
-        plt.imshow(rgb_frame)
+        """
+        fig, axes = plt.subplots(1, 2)
+        axes[0].imshow(rgb_frame)
+        axes[1].imshow(depth_offset - depth_frame, cmap=plt.cm.gray)
+        """
                 
         layers = []
         for i in range(d_responses.shape[2] - 1):
+            
+            #plt.figure()
+            #plt.imshow(rgb_frame)
+                
             
             """
             plt.figure()
@@ -203,28 +247,83 @@ if __name__ == '__main__':
             contours = measure.find_contours(layer, 0.5)
             #print('{} contours'.format(len(contours)))
             
-            contours = [c[:, ::-1] for c in contours]
+            contours = [c[:,::-1] for c in contours]
+            fn = '/Users/jonathan/block_coords/{}.csv'.format(frame_num)
+            with open(fn, 'w') as csvfile:
+                csvwriter = csv.writer(csvfile)
             
-            for contour in contours:
-                rr, cc = draw.polygon(contour[:,1], contour[:,0])
-                # FIXME: Do SVD on interior of contour, not edges
-                U, S, V = la.svd(contour)
-                mean = contour.mean(axis=0)
-                std = contour.std(axis=0)   # I'm treating this like an average radius
-                if la.norm(std) < 5:
-                    continue
-                #print('  {} | {} | {}'.format(contour.mean(axis=0), la.norm(std), S))
-                cont_int = contour.astype(int)
-                colors = c_responses[rr,cc,:-1].sum(axis=0)
-                color = rgb_values[colors.argmax()]
-                #colors = colors[colors[:,0,] != 128, :]
-                plt.plot(contour[:, 0], contour[:, 1], linewidth=2, color=color)
-                #plt.plot(cc, rr, linewidth=2, color=color)
-                line = np.vstack((mean, mean + 10 * V[0,:]))
-                plt.plot(line[:,0], line[:,1], color='r')
-                line = np.vstack((mean, mean + 10 * V[1,:]))
-                plt.plot(line[:,0], line[:,1], color='b')
-            
+                for contour in contours:
+                    
+                    interior_pts = np.column_stack(draw.polygon(contour[:,1], contour[:,0]))
+                    depth_pts = depth_frame[interior_pts[:,0], interior_pts[:,1]]
+                    block_depth = (depth_offset - depth_pts).mean()
+                    mean = interior_pts.mean(axis=0)
+                    # I'm treating this like an average radius
+                    std = interior_pts.std(axis=0)
+                    if la.norm(std) < 5:
+                        continue
+                    interior_centered = interior_pts - np.vstack(interior_pts.shape[0] * (mean,))
+                    U, S, V = la.svd(interior_centered)
+                    D = np.zeros((U.shape[0], V.shape[0]))
+                    for i in range(S.shape[0]):
+                        D[i,i] = S[i]
+                    rotated_contour = U.dot(D) + np.vstack(interior_pts.shape[0] * (mean,))
+                    block_length = rotated_contour[:,0].max() - rotated_contour[:,0].min()
+                    block_width = rotated_contour[:,1].max() - rotated_contour[:,1].min()
+                    ratio = block_length / block_width
+                    
+                    cont_int = contour.astype(int)
+                    colors = c_responses[interior_pts[:,0],interior_pts[:,1],:-1].sum(axis=0)
+                    color = rgb_values[colors.argmax()]
+                    
+                    #axes[1].plot(contour[:, 0], contour[:, 1], color=color)
+                    
+                    pixel_to_metric = block_depth / f    # mm / pixel
+                    if pixel_to_metric * block_length > 1.5 * block_length_metric:
+                        continue
+                    
+                    if ratio < 1:
+                        block_type = 'impossible'
+                    elif ratio < 1.5:
+                        # FIXME: Rotate corners by 45 degrees in this case
+                        block_type = 'square'
+                        diag_len = 0.5 * (block_length + block_width)
+                        block_length = diag_len / 2 ** 0.5
+                        block_width = diag_len / 2 ** 0.5
+                    elif ratio < 3:
+                        block_type = '2:1 rect'
+                        block_width = block_length / 2
+                    else:
+                        block_type = '4:1 rect'
+                        block_width = block_length / 4
+                    
+                    theta = np.rad2deg(np.arccos(V[0,0]))
+                    row = list(pixel_to_metric * mean) + [block_depth, theta] \
+                        + [block_width, block_length, block_height_metric] + list(color)
+                    csvwriter.writerow(row)
+                    
+                    corners = np.array([[ block_length,  block_width],
+                                        [ block_length, -block_width],
+                                        [-block_length, -block_width],
+                                        [-block_length,  block_width]]) / 2
+                    corners = corners.dot(V) + np.vstack(corners.shape[0] * (mean,))
+                    corners = np.vstack((corners, corners[0:1,:]))
+                    
+                    #print('Length: {:.2f}  |  Width: {:.2f}  |  Ratio: {:.2f}  |  {}'.format(block_length, block_width, ratio, block_type))
+                    
+                    #print('  {} | {} | {}'.format(contour.mean(axis=0),
+                    #    la.norm(std), S))
+                    
+                    #colors = colors[colors[:,0,] != 128, :]
+                    #axes[0].plot(corners[:, 1], corners[:, 0], color=color)
+                    
+                    #plt.plot(cc, rr, linewidth=2, color=color)
+                    #line = np.vstack((mean, mean + 10 * V[0,:]))
+                    #plt.plot(line[:,1], line[:,0], color='r')
+                    #line = np.vstack((mean, mean + 10 * V[1,:]))
+                    #plt.plot(line[:,1], line[:,0], color='b')
+                
+            """
             # Convex hull
             hull = morphology.convex_hull_object(layer_edges)
             objects = c_img.copy()
@@ -233,16 +332,25 @@ if __name__ == '__main__':
             #plt.imshow(objects, cmap=plt.cm.gray)
             
             layers.append(objects)
-        
+            """
+        """
         layers.append(np.zeros(layers[-1].shape, dtype='uint8'))
         layer_frame = np.vstack((np.hstack(tuple(layers[0:2])),
                                  np.hstack(tuple(layers[2:4]))))
+        """
+        """
+        for ax in axes:
+            ax.axis('image')
+            ax.set_xticks([])
+            ax.set_yticks([])
+        
         #plt.figure()
         #plt.imshow(layer_frame, cmap=plt.cm.gray); plt.show()
         _, fn = os.path.split(rgb_path)
         #io.imsave(os.path.join(c.paths['working'], fn), layer_frame)
         plt.savefig(os.path.join(c.paths['working'], fn), format='png')
         plt.close()
+        """
         
     
     """
@@ -259,57 +367,4 @@ if __name__ == '__main__':
     subprocess.call(make_video)
     """
     
-    block_1_corners = np.array([[120, 101], [119, 87], [133, 85], [134, 100]])
-    block_2_corners = np.array([[134, 100], [133, 85], [147, 84], [148, 99]])
-    block_3_corners = np.array([[149, 97], [148, 83], [161, 82], [162, 99]])
-    block_4_corners = np.array([[164, 96], [163, 81], [176, 80], [177, 95]])
-    side_lens = np.zeros(block_1_corners.shape[0])
-    side_lens[:-1] = (np.diff(block_1_corners, axis=0) ** 2).sum(axis=1) ** 0.5
-    side_lens[-1] = ((block_1_corners[-1,:] - block_1_corners[0,:]) ** 2).sum() ** 0.5
-    z_m = 600   # mm
     
-    # Side length of a 2x2 DUPLO brick (from stack excange link below)
-    # http://bricks.stackexchange.com/questions/1588/
-    #   what-are-the-exact-dimensions-of-a-duplo-brick
-    block_width = 31.8  # mm
-    block_height = 19.2 # mm
-    
-    # Average focal length assuming a pinhole camera
-    f = (side_lens * (z_m - block_height) / block_width).mean() # pixels
-    
-    # Block corner locations in 3D assuming coordinate frames of metric space
-    # and the image plane are aligned
-    metric_corners = block_1_corners * (z_m - block_height) / f
-    
-    # It looks like a model for the depth camera is:
-    #   depth = max_depth - intensity
-    # where max_depth is about 636
-    depth_offset = 636
-    
-    """
-    num_corners = block_1_corners.shape[0]
-    center = np.array(rgb_frame.shape[0:2])
-    Y = z_m * (block_1_corners - 0.5 * np.vstack(num_corners * (center,)))
-    X = np.array([[-1, 1], [1, 1], [1, -1], [-1, -1]])
-    
-    mean_x = X.mean(axis=0)
-    mean_y = Y.mean(axis=0)
-    
-    Y_centered = Y - np.vstack(Y.shape[0] * (mean_y,))
-    X_centered = X - np.vstack(X.shape[0] * (mean_x,))
-    
-    var_x = (X_centered ** 2).sum(axis=1).mean()
-    var_y = (Y_centered ** 2).sum(axis=1).mean()
-    
-    cov_xy = (Y_centered.T).dot(X_centered)
-    U, D, V = la.svd(cov_xy)
-    
-    S = np.eye(2) if la.det(cov_xy) > 0 else np.diag(np.array([1, -1]))
-    
-    R = U.dot(S).dot(V.T)
-    c = np.diag(D).dot(S).trace() / var_x
-    t = mean_y - c * R.dot(mean_x)
-    
-    f = c / (block_width / 2)
-    block_center = t / f
-    """
---
+++ src/renderworldstate.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+renderworldstate.py
+  Load block positions and orientations and draw them in blender.
+
+AUTHOR
+  Jonathan D. Jones
+"""
+
+import bpy
+import csv
+
+fn = '/Users/jonathan/block_coords/50.csv'
+with open(fn) as csvfile:
+    csvreader = csv.reader(csvfile)
+    
+    #"""
+    i = 0
+    #i = 10.0
+    #bpy.ops.mesh.primitive_cube_add(radius=15.9 / 10)
+    #ob = bpy.context.object
+    #ob.color = (255, 0, 0, 0)
+    #me = ob.data
+    #ob.keyframe_insert(data_path='location', frame=i)
+    #ob.keyframe_insert(data_path='rotation_euler', frame=i)
+    objects = []
+    for row in csvreader:
+        
+        position = tuple(float(x) / 10 for x in row[0:3])
+        angle = (0, 0, float(row[3]))
+        r_width, r_length, r_height = tuple(float(x) / 2 / 10 for x in row[4:7])
+        color = tuple(int(x) for x in row[7:10])
+        
+        #c = bpy.ops.mesh.primitive_cube_add(radius=15.9, location=position,
+        #                                    rotation=angle)
+        # Create mesh and object
+        meshName = "block_mesh_{}".format(i)
+        obName = "block_{}".format(i)
+        me = bpy.data.meshes.new(meshName)
+        ob = bpy.data.objects.new(obName, me)
+        ob.show_name = True
+        
+        # Link object to scene and make active
+        scn = bpy.context.scene
+        scn.objects.link(ob)
+        scn.objects.active = ob
+        ob.select = True
+        
+        #r_height = 19.2 / 2 / 10
+        #r_width = 31.8 / 2 / 10
+        #r_length = 2 * r_width
+        verts = [( r_width,  r_length,  r_height), (-r_width,  r_length,  r_height),
+                 (-r_width, -r_length,  r_height), ( r_width, -r_length,  r_height),
+                 ( r_width,  r_length, -r_height), (-r_width,  r_length, -r_height),
+                 (-r_width, -r_length, -r_height), ( r_width, -r_length, -r_height),]
+        faces = [(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 7, 3), (1, 5, 6, 2),
+                 (0, 4, 5, 1), (3, 7, 6, 2)]
+        me.from_pydata(verts, [], faces)
+        me.update()
+        
+        ob.location = position
+        ob.rotation_euler = angle
+        ob.keyframe_insert(data_path='location', frame=i)
+        ob.keyframe_insert(data_path='rotation_euler', frame=i)
+        
+        objects.append(ob)
+        
+        i += 1
