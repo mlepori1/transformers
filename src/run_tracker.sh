87a292114872075d18f0246ef6ea29ddd7e34cde
Jonathan Jones <jones.jonathan.david@gmail.com>
2016-08-10 21:04:16 -0400

    added some files I forgot

---
+++ src/run_tracker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# run_tracker.sh
+#   set up config files required for the tracking program and then run it.
+#
+# HISTORY
+#   2016-08-05: Created by Jonathan D. Jones
+#
+
+IMAGE_PATH='../working/gl-render/'
+
+ls $IMAGE_PATH*'.png' > $IMAGE_PATH"png-files.txt"
+
+#./main
